c61c4e22a3f28120f5c5586866c40966429cf936
marketbytes-devops <marketbytesdevops@gmail.com>
2026-04-03 17:59:15 +0530

    Updated V1

--- backend/operation/views.py
+++ backend/operation/views.py
@@ -2,12 +2,7 @@ from rest_framework import viewsets, status, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models import Q
-from rest_framework import viewsets, status, filters
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from authapp.permissions import HasPermission
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Q
 import datetime
